e6f67a5e72ba0629fb4c204cf219e9814fbe9647
Charlie Hall <chall70101@gmail.com>
2024-10-03 18:34:43 -0600

    fix

--- servicePipeline.groovy
+++ servicePipeline.groovy
@@ -9,56 +9,58 @@ def call(){
     }
 
 def tagging(){
-    stages {
-        stage('Authenticate with GCR') {
-            steps {
-                withCredentials([file(credentialsId: 'gcr-json-key', variable: 'GCR_KEYFILE')]) {
-                    sh '''
+    return {
+        stages {
+            stage('Authenticate with GCR') {
+                steps {
+                    withCredentials([file(credentialsId: 'gcr-json-key', variable: 'GCR_KEYFILE')]) {
+                        sh '''
                         gcloud auth activate-service-account --key-file=${GCR_KEYFILE}
                         gcloud auth configure-docker --quiet
                     '''
-                }
-            }
-        }
-
-        stage('Manage Image Tags in GCR') {
-            steps {
-                script {
-                    if (params.ACTION == 'ADD') {
-                        // Add a new tag to the image
-                        sh """
-                            gcloud container images add-tag ${GCR_IMAGE}@${params.IMAGE_DIGEST} ${GCR_IMAGE}:${params.NEW_TAG} --quiet
-                        """
-                        echo "Tag ${params.NEW_TAG} added to image with digest ${params.IMAGE_DIGEST}"
-                    } else if (params.ACTION == 'REMOVE') {
-                        // Remove a tag from the image
-                        sh """
-                            gcloud container images untag ${GCR_IMAGE}:${params.NEW_TAG} --quiet
-                        """
-                        echo "Tag ${params.NEW_TAG} removed from image."
                     }
                 }
             }
-        }
-        stage('Update bitbucket tags'){
-            steps {
-                script {
-                    if (params.ACTION == 'ADD') {
-                        sh '''
+
+            stage('Manage Image Tags in GCR') {
+                steps {
+                    script {
+                        if (params.ACTION == 'ADD') {
+                            // Add a new tag to the image
+                            sh """
+                            gcloud container images add-tag ${GCR_IMAGE}@${params.IMAGE_DIGEST} ${GCR_IMAGE}:${params.NEW_TAG} --quiet
+                        """
+                            echo "Tag ${params.NEW_TAG} added to image with digest ${params.IMAGE_DIGEST}"
+                        } else if (params.ACTION == 'REMOVE') {
+                            // Remove a tag from the image
+                            sh """
+                            gcloud container images untag ${GCR_IMAGE}:${params.NEW_TAG} --quiet
+                        """
+                            echo "Tag ${params.NEW_TAG} removed from image."
+                        }
+                    }
+                }
+            }
+            stage('Update bitbucket tags') {
+                steps {
+                    script {
+                        if (params.ACTION == 'ADD') {
+                            sh '''
                             git tag ${params.NEW_TAG}
                             git push origin ${params.NEW_TAG}
                         '''
-                        echo "Tag ${params.NEW_TAG} added and pushed to Bitbucket."
-                    } else if (params.ACTION == 'REMOVE') {
-                        sh '''
+                            echo "Tag ${params.NEW_TAG} added and pushed to Bitbucket."
+                        } else if (params.ACTION == 'REMOVE') {
+                            sh '''
                             git tag -d ${params.NEW_TAG}
                             git push origin :refs/tags/${params.NEW_TAG}
                         '''
-                        echo "Tag ${params.NEW_TAG} removed from Bitbucket."
+                            echo "Tag ${params.NEW_TAG} removed from Bitbucket."
+                        }
                     }
                 }
-            }
 
+            }
         }
     }
 }
